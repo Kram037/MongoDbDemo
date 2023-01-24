43e29207cdbf06089158b963b1f9236c5f8f6004
Kram037 <defenzomarco@gmail.com>
2023-01-24 19:00:32 +0100

    initial commit

---
+++ src/test/java/co/develhope/MongoDbDemo/MongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.MongoDbDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
